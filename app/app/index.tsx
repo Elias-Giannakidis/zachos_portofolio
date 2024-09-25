e6a1e53892cb94ec58dc7591ce4f09fcd998acbd
egiannakidis <hl.giannakidis@gmail.com>
2024-09-25 17:03:40 +0300

    const-colors

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,7 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Navigation } from "@/components/navigation/Navigation";
 
 export default function Index() {
   return (
-    <View/>
+    <View>
+      <Navigation/>
+    </View>
   );
 }
