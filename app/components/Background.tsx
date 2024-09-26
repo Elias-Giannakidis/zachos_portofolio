7975b9f0ce039d902d27f2cd57d1d4bf51025bd3
Elias Giannakidis <hl.giannakidis@gmail.com>
2024-09-26 14:47:29 +0300

    backgroud-completed

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,14 +1,38 @@
 import {Colors} from "../constants/Colors"
-
+import React, {useState} from 'react'
 
 export function Background() {
 
-    const style = {
+    const [isHovered, setIsHovered] = useState(false)
 
+    const imageStyle = {
+        "width": "100%",
+        "height": "100%",
+        "backgroundImage": "url(../images/background.jpg)",
+        "alignItems": "center",
+        "display": "flex",
+        "justifyContent": "center",
+    }
+
+    const foregroundStyle = {
+        "width": "94%",
+        "height": "94%",
+        "background": `radial-gradient(circle, ${Colors.white} 0%, ${Colors.white} 30%, ${Colors.gray} 100%)`,
+        "borderRadius": "10px",
+        "borderWidth": "8px",
+        "boxShadow": isHovered ? "0px 0px 50px 30px" : "0px 0px 30px 20px",
+        "transform": isHovered ? 'scale(1.01)' : 'scale(1)',
+        "transition": "box-shadow 0.6s ease, transform 0.6s ease"
     }
 
 
     return (
-        <img src="../images/background.jpg" alt="background"></img>
+        <div style={imageStyle}>
+            <div 
+                style={foregroundStyle}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                ></div>
+        </div>
     )
 }
--- app/constants/Colors.ts
+++ app/constants/Colors.ts
@@ -3,5 +3,6 @@ export const Colors = {
   yellow: '#ffff00',
   green: '#009933',
   black: '#000000',
-  white: '#ffffff'
+  white: '#ffffff',
+  gray: '#999999'
 };
